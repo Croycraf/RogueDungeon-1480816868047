d81c94d65440ca604d20d2d31067e48de834cce9
Connor Roycraft <croycraf@purdue.edu>
2016-12-03 21:59:34 -0500

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -13,58 +13,11 @@
 	$result = $conn->query("SELECT userName, score, floor FROM highscores ORDER BY score DESC");
 	$rank = 1;
 	
-	if (mysql_num_rows($result)) {
-		/*
-		while ($row = mysql_fetch_assoc($result)) {
-			echo "{$rank} {$row['userName']} {$row['score']} {$row['floor']}";
-			$rank++;
-		}
-		*/
+	while ($row = mysql_fetch_assoc($result)) {
+		echo "{$rank} {$row['userName']} {$row['score']} {$row['floor']}";
+		$rank++;
 	}
+
 	$conn->close();
-
-/*
-function getScores() {
-		$servername = "us-cdbr-iron-east-04.cleardb.net";
-		$username = "b8e8d5c50f2a55";
-		$password = "ea36a1fa";
-
-		$conn = mysqli_connect($servername, $username, $password);
-
-		if (!$conn) {
-		    die("Connection failed: " . mysqli_connect_error());
-		}
-		echo "Connected successfully";
-		
-		
-        $result = mysql_query("SELECT userName, score, floor FROM highscores ORDER BY score DESC");
-        $rank = 1;
-
-        if (mysql_num_rows($result)) {
-            while ($row = mysql_fetch_assoc($result)) {
-                echo "<td>{$rank}</td>
-					  <td>{$row['userName']}</td>
-                      <td>{$row['score']}</td>
-                      <td>{$row['floor']}</td>";
-
-                $rank++;
-            }
-        }
-        mysqli_close($conn);
-}
-
-if($_GET['do'] == "getScores") {
-
-	getScores();
-
-}
- */  
-/*
-     "name": "ad_78271414726dba7",
-        "hostname": "us-cdbr-iron-east-04.cleardb.net",
-        "port": "3306",
-        "username": "b8e8d5c50f2a55",
-        "password": "ea36a1fa"
-*/
 
 ?>
